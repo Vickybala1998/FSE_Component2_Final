0571d344f7c12bd23ee0ae8cdfe8b5d192ed08a1
Vickybala1998 <51534407+Vickybala1998@users.noreply.github.com>
2022-03-01 14:05:43 +0530

    Add files via upload

---
+++ Component_3/ServiceBusConsumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tweetServiceBus_Consumer
+{
+    class Program
+    {
+
+        //private static ServiceBusClient _client;
+        //private static ServiceBusProcessor _processor;
+        private const string QUEUE_NAME = "tweetapp";
+        private static IQueueClient _client;
+        private const string CONNECTION_STRING = "Endpoint=sb://tweetappproducer1.servicebus.windows.net/;SharedAccessKeyName=Listner;SharedAccessKey=bnErI0QTvv/N6ow/szgGmNTWRpmQhrpSYsPl0D0f/54=;";
+        static void Main(string[] args)
+        {
+            register();
+        }
+
+        public static void register()
+        {
+            _client = new QueueClient(CONNECTION_STRING, QUEUE_NAME);
+            var _serviceBusProcessOptions = new MessageHandlerOptions(ExceptionMethod)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            _client.RegisterMessageHandler(ExecuteMessageProcessing, _serviceBusProcessOptions);
+            Console.WriteLine("started");
+            Console.Read();
+        }
+
+        private static async Task ExecuteMessageProcessing(Message message, CancellationToken arg2)
+        {
+             var result = JsonConvert.DeserializeObject<tweet>(Encoding.UTF8.GetString(message.Body));
+            Console.WriteLine("message "+result.Message+"is send by"+result.User_Name);
+            await _client.CompleteAsync(message.SystemProperties.LockToken);
+        }
+        private static async Task ExceptionMethod(ExceptionReceivedEventArgs arg)
+        {
+            await Task.Run(() =>
+           Console.WriteLine($"Error occured. Error is {arg.Exception.Message}")
+           );
+        }
+
+    }
+}
---
+++ Component_3/ServiceBusConsumer/tweet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweetServiceBus_Consumer
+{
+   public  class tweet
+    {
+        public string Id { get; set; }
+        public string User_Name { get; set; }
+        public string Message { get; set; }
+        public string Posted_On { get; set; }
+        public int No_of_Likes { get; set; }
+        public int No_of_DisLikes { get; set; }
+        public string Reply_Message { get; set; }
+        public List<Dictionary<string, string>> RepliesList { get; set; }
+    }
+}
